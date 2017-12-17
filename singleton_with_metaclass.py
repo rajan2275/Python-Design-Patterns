95bfe5359c27029e27c045c5ef1e02f95e7355d8
Rajan <rajan2275@gmail.com>
2017-12-17 13:36:27 +0000

    singleton with metaclass.

---
+++ singleton_with_metaclass.py
@@ -0,0 +1,21 @@
+
+# Implement Singleton using Metaclass
+# ------------------------------------
+class MetaSingleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton,cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Logger(metaclass=MetaSingleton):
+    pass
+
+logger1 = Logger()
+logger2 = Logger()
+
+# Both reffer to same object.
+# ----------------------------
+print(logger1, logger2)
