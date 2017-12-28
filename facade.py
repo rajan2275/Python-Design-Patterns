b7973150cc77402ffdede4b09dfb8ad64eb1ea5b
Rajan <rajan2275@gmail.com>
2017-12-28 22:31:27 +0000

    facade pattern

---
+++ facade.py
@@ -0,0 +1,55 @@
+# -----------------------------------------------------
+# Facade Implementation.
+# -----------------------------------------------------
+class ResourceManager:
+    def __init__(self):
+        print("Update all resources.")
+
+
+    def update_resources(self):
+        self.reportDb = ReportDb()
+        self.reportDb.update()
+
+        self.intranetDb = InntranetDb()
+        self.intranetDb.save()
+
+        self.site = Site()
+        self.site.update()
+
+        self.Api = Api()
+        self.Api.post()
+
+class ReportDb:
+    def __init__(self):
+        print ("ReportDb called.")
+
+    def update(self):
+        print("ReportDb updated.")
+
+class InntranetDb:
+    def __init__(self):
+        print("InntranetDb called.")
+
+    def save(self):
+        print("InntranetDb updated.")
+
+class Site:
+    def __init__(self):
+        print("SiteDb called.")
+
+    def update(self):
+        print("Site updated.")
+
+class Api:
+    def __init__(self):
+        print("Api called.")
+
+    def post(self):
+        print("Api post called.")
+
+
+# ------------
+# CLient code
+# ------------
+resource_manager =  ResourceManager()
+resource_manager.update_resources()
