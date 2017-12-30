62ea98d7ac8d95b8ae52ac25db4f9ce39d4b63a0
Rajan <rajan2275@gmail.com>
2017-12-30 22:53:58 +0000

    command pattern example.

---
+++ command_example_1.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+#---------------------------
+#  Abstract Command class
+#---------------------------
+class Order(ABC):
+
+    @abstractmethod
+    def process(self):
+        pass
+
+#---------------------------
+#  Concrete Command class
+#---------------------------
+class BuyStock(Order):
+    def __init__(self, stock):
+        self.stock = stock
+
+    def process(self):
+        self.stock.buy()
+
+#---------------------------
+#  Concrete Command class
+#---------------------------
+class SellStock(Order):
+    def __init__(self, stock):
+        self.stock = stock
+
+    def process(self):
+        self.stock.sell()
+
+#---------------------------
+#  Receiver class
+#---------------------------
+class Trade:
+    def buy(self):
+        print("Stock buy request placed.")
+
+    def sell(self):
+        print("Stock sell request placed.")
+
+
+class Invoker:
+    def __init__(self):
+        self._queue = []
+
+    def process_order(self, order):
+        self._queue.append(order)
+        order.process()
+
+trade = Trade()
+buy_stock = BuyStock(trade)
+sell_stock = SellStock(trade)
+
+invoker = Invoker()
+invoker.process_order(buy_stock)
+invoker.process_order(sell_stock)
