907e1517d6c4bf1472483cb4a99226fab3a36062
Rajan Sharma <rajan2275@gmail.com>
2018-01-14 12:52:30 +0000

    Update command.py
    Command Pattern.

--- Behavioral/command.py
+++ Behavioral/command.py
@@ -1,3 +1,9 @@
+# ----------------
+# Command Pattern
+# ----------------
+# Encapsulate a request as an object, thereby allowing for the parameterization of clients with different requests, 
+# and the queuing or logging of requests. It also allows for the support of undoable operations.
+
 from abc import ABC, abstractmethod
 
 class Command(ABC):
