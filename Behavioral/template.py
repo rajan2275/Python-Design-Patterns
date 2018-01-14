bb3878344246beed13df0960b2499ee0ee573351
Rajan Sharma <rajan2275@gmail.com>
2018-01-14 13:00:27 +0000

    Template design pattern.

--- Behavioral/template.py
+++ Behavioral/template.py
@@ -1,3 +1,9 @@
+# ------------------------------
+# Template Design Pattern.
+# ------------------------------
+# Define the skeleton of an algorithm in an operation, deferring some steps to subclasses. Template method lets subclasses 
+# redefine certain steps of an algorithm without changing the algorithm's structure
+
 from abc import ABC, abstractmethod
 
 class Template(ABC):
