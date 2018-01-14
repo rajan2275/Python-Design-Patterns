8cf5bcc87a1536ad8310f1b6a4f0883c18b50a5b
Rajan Sharma <rajan2275@gmail.com>
2018-01-14 12:58:47 +0000

    State design pattern.

--- Behavioral/state.py
+++ Behavioral/state.py
@@ -1,3 +1,8 @@
+# ------------------------------
+# State Design Pattern
+# ------------------------------
+# Allow an object to alter its behavior when its internal state changes. The object will appear to change its class.
+
 from abc import ABC, abstractmethod
 
 class State(ABC):
