e4cf3893eae8c6da461d17a9d388a164ae4e40a1
Rajan Sharma <rajan2275@gmail.com>
2018-01-14 12:57:30 +0000

    Observer design pattern.

--- Behavioral/observer.py
+++ Behavioral/observer.py
@@ -1,6 +1,8 @@
 # ------------------------------
 # Observer Design Pattern
 # ------------------------------
+# Define a one-to-many dependency between objects
+# where a state change in one object results in all its dependents being notified and updated automatically.
 
 
 class Subject:
