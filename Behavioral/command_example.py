184734e1f1295e7b4d787af27ed1a89c1d028d90
Rajan Sharma <rajan2275@gmail.com>
2018-01-14 12:54:47 +0000

    Command pattern example.

--- Behavioral/command_example.py
+++ Behavioral/command_example.py
@@ -1,3 +1,9 @@
+# ----------------
+# Command Pattern.
+# ----------------
+# Encapsulate a request as an object, thereby allowing for the parameterization of clients with different requests, 
+# and the queuing or logging of requests. It also allows for the support of undoable operations.
+
 class Installer:
     def __init__(self, source, dir):
         self.options = []
