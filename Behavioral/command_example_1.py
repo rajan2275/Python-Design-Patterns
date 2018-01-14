22f6d6355eef26d4f8fcf8379c7959f4f3eeb9fc
Rajan Sharma <rajan2275@gmail.com>
2018-01-14 12:55:56 +0000

    Command pattern example.

--- Behavioral/command_example_1.py
+++ Behavioral/command_example_1.py
@@ -1,3 +1,9 @@
+# -----------------------
+# Command pattern example.
+# ------------------------
+# Encapsulate a request as an object, thereby allowing for the parameterization of clients with different requests, 
+# and the queuing or logging of requests. It also allows for the support of undoable operations.
+
 from abc import ABC, abstractmethod
 
 #---------------------------
