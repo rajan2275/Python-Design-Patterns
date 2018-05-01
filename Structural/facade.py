66bb05bd3a6c91b77f2d8de7efbcb8256bae7ef2
Rajan Sharma <rajan2275@gmail.com>
2018-05-01 13:35:32 +0100

    Update facade.py

--- Structural/facade.py
+++ Structural/facade.py
@@ -1,6 +1,10 @@
-# -----------------------------------------------------
+# ------------------------------------------------------------------------------
 # Facade Implementation.
-# -----------------------------------------------------
+#--------------------------------------------------------------------------------
+# Provide a unified interface to a set of interfaces in a subsystem. 
+# Façade defines a higher-level interface that makes the subsystem easier to use.
+# -------------------------------------------------------------------------------
+
 class ResourceManager:
     def __init__(self):
         print("Update all resources.")
